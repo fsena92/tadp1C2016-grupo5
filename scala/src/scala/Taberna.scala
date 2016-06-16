0d6adcf9fc6eda94581772d4ad370e85dcb55e54
fedehipper <federicohipper@gmail.com>
2016-06-16 17:21:50 -0300

    terminado tp

--- scala/src/scala/Taberna.scala
+++ scala/src/scala/Taberna.scala
@@ -1,8 +1,9 @@
 package scala
 
+import scala.util.control.Breaks._
+
 class Taberna(var misiones: Set[Mision]) {
   
-  //al equipo le aplica dos misiones y agarra la mejor, si hay una te devuelve la semilla papá!!
   def elegirMision(criterio: (Equipo, Equipo) => Boolean, unEquipo: Equipo): Mision = {
       misiones.foldLeft(misiones.head){(mision1, mision2) => 
         if (criterio(unEquipo.realizarMision(mision1), unEquipo.realizarMision(mision2))) mision1 
@@ -10,16 +11,12 @@ class Taberna(var misiones: Set[Mision]) {
       }
   }
   
-  // intenta realizar todas las misiones
   def entrenar(unEquipo: Equipo, unCriterio: (Equipo, Equipo) => Boolean) = {
     var equipoIntermedio = unEquipo
     while(misiones.size > 0) {
       equipoIntermedio = unEquipo.realizarMision(elegirMision(unCriterio, unEquipo))
-      if(equipoIntermedio.tareaFallida.isDefined)
-        //break ---> hacerlo
-        0
-      else
-        misiones = misiones.filterNot(m => m eq elegirMision(unCriterio, unEquipo))
+      if(equipoIntermedio.tareaFallida.isDefined) break
+      else misiones = misiones.filterNot(m => m eq elegirMision(unCriterio, unEquipo))
     }
     equipoIntermedio
   }
