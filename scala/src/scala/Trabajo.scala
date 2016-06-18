4cf079d031f77dd17a915dea7cd6955bcadf4c83
fedehipper <federicohipper@gmail.com>
2016-06-18 19:46:35 -0300

    correcciones

--- scala/src/scala/Trabajo.scala
+++ scala/src/scala/Trabajo.scala
@@ -1,15 +1,23 @@
 package scala
 
-abstract class Trabajo(val HP: Double = 0, val fuerza: Double, 
-    val velocidad: Double, val inteligencia: Double, val statPrincipal: Heroe => Double) {
+abstract class Trabajo(val HP: Double = 0, val fuerza: Double = 0, 
+    val velocidad: Double = 0, val inteligencia: Double = 0) {
   
   def fuerza(valor: Double):Double = valor + fuerza
   def HP(valor: Double): Double = valor + HP
   def velocidad(valor: Double): Double = valor + velocidad
   def inteligencia(valor: Double): Double = valor + inteligencia
   
+  def statPrincipal(heroe: Heroe):Double
+  
 }
 
-case object Guerrero extends Trabajo(10, 15, 0, -10, _.fuerzaFinal)
-case object Mago extends Trabajo(0, 20, 0, 20, _.inteligenciaFinal)
-case object Ladron extends Trabajo (-5, 0, 10, 0, _.velocidadFinal)
+case object Guerrero extends Trabajo(HP = 10, fuerza = 15, inteligencia = -10) {
+  def statPrincipal(heroe: Heroe) = heroe.fuerzaFinal
+}
+case object Mago extends Trabajo(fuerza = 20, inteligencia = 20) {
+  def statPrincipal(heroe: Heroe) = heroe.inteligenciaFinal
+}
+case object Ladron extends Trabajo (HP = -5, velocidad = 10) {
+  def statPrincipal(heroe: Heroe) = heroe.velocidadFinal
+}
