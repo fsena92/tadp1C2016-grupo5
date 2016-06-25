86687411401eec798fe02d98c47cf3369577d628
fedehipper <federicohipper@gmail.com>
2016-06-25 03:08:36 -0300

    simplificacion de codigo

--- scala/src/scala/Inventario.scala
+++ scala/src/scala/Inventario.scala
@@ -37,18 +37,10 @@ case class Inventario(val items: List[Item] = Nil) {
     items.foldLeft(valor)((v, item) => i(item, heroe, v))  
   }
   
-  def fuerzaFinal(heroe: Heroe, valor: Double) = obtenerDeItems(heroe, valor)(_ fuerza(_, _))
-  def HPFinal(heroe: Heroe, valor: Double) = obtenerDeItems(heroe, valor)(_ HP(_, _))
-  def velocidadFinal(heroe: Heroe, valor: Double) = obtenerDeItems(heroe, valor)(_ velocidad(_, _))
-  def inteligenciaFinal(heroe: Heroe, valor: Double) = obtenerDeItems(heroe, valor)(_ inteligencia(_, _))
- 
-  //opcion 2
-//  def fuerzaFinal = obtenerDeItems(_:Heroe, _:Double)(_ fuerza(_, _))
-//  def HPFinal = obtenerDeItems(_:Heroe, _:Double)(_ HP(_, _))
-//  def velocidadFinal = obtenerDeItems(_:Heroe, _:Double)(_ velocidad(_, _))
-//  def inteligenciaFinal = obtenerDeItems(_:Heroe, _:Double)(_ inteligencia(_, _))
-//  
-  
+  def fuerzaFinal = obtenerDeItems(_:Heroe, _:Double)(_ fuerza(_, _))
+  def HPFinal = obtenerDeItems(_:Heroe, _:Double)(_ HP(_, _))
+  def velocidadFinal = obtenerDeItems(_:Heroe, _:Double)(_ velocidad(_, _))
+  def inteligenciaFinal = obtenerDeItems(_:Heroe, _:Double)(_ inteligencia(_, _))
   
   def cantidadItems = items.size
 }
